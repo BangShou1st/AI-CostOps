8c0e07f65775b36458ffb25cc466485a6a07f490
BangShou1st <3296249574@qq.com>
2026-08-12 18:39:43 +0800

    chore(backend): bootstrap Spring Boot application

---
+++ backend/src/test/java/com/aicostops/AiCostOpsApplicationTest.java
@@ -0,0 +1,12 @@
+package com.aicostops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "spring.flyway.enabled=false")
+class AiCostOpsApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
